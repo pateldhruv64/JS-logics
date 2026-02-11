e447ad7abfdead567c09c13117d96be78df1c4d9
pateldhruv64 <pateldhruv64922@gmail.com>
2026-02-11 00:44:09 -0600

    noting

--- Pre-react/react/src/App.jsx
+++ Pre-react/react/src/App.jsx
@@ -1,20 +1,15 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-  
-
   return (
     <>
-    <div class='w-full h-screen justify-center  flex items-center bg-cyan-400'>
-      <div class='justify-center  flex items-center bg-amber-500 w-100 h-100 '>
-          
-          <div class='justify-center items-center bg-amber-900  hover:bg-amber-200 transition delay-200'>hello </div>
+      <div class="w-full h-screen justify-center  flex items-center bg-cyan-400  hover:bg-emerald-300 transition delay-700 ease-in">
+        <div class="justify-center  flex items-center bg-amber-500 w-100 h-100  hover:bg-pink-300 transition delay-500 ease-in">
+          <div class="justify-center items-center bg-amber-900  hover:bg-amber-200 transition delay-200"></div>
         </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
