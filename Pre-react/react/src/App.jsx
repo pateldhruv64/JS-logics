66c043e088ec62683921701ab1098e8b0097413f
pateldhruv64 <pateldhruv64922@gmail.com>
2026-02-11 12:01:53 +0530

    c

--- Pre-react/react/src/App.jsx
+++ Pre-react/react/src/App.jsx
@@ -6,9 +6,13 @@ function App() {
 
   return (
     <>
-<button class="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
-
-<div>hello</div>
+    <div class='w-full h-screen justify-center  flex items-center bg-cyan-400'>
+      <div class='justify-center  flex items-center bg-amber-500 w-100 h-100 '>
+          
+          <div class='justify-center items-center bg-amber-900  hover:bg-amber-200 transition delay-200'>hello </div>
+        </div>
+      </div>
+      
     </>
   )
 }
